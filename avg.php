39492251d40a2d0f0cff4e3a78d2e7f261bd8e91
Oliwia-A <oliwia.antosiewicz1@gmail.com>
2025-06-17 16:45:56 +0200

    srednia wazona

--- avg.php
+++ avg.php
@@ -5,3 +5,25 @@ function srednia($liczby) {
     }
     return array_sum($liczby) / count($liczby);
 }
+<?php
+function srednia($liczby) {
+    if (empty($liczby)) {
+        return 0;
+    }
+    return array_sum($liczby) / count($liczby);
+}
+
+function srednia_wazona($liczby, $wagi) {
+    if (empty($liczby) || empty($wagi) || count($liczby) !== count($wagi)) {
+        return 0;
+    }
+
+    $suma = 0;
+    $suma_wag = array_sum($wagi);
+
+    for ($i = 0; $i < count($liczby); $i++) {
+        $suma += $liczby[$i] * $wagi[$i];
+    }
+
+    return $suma_wag ? $suma / $suma_wag : 0;
+}
