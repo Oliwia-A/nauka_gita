464526248eb83394e667a270668c090ab34a1bac
Oliwia-A <oliwia.antosiewicz1@gmail.com>
2025-06-17 16:43:26 +0200

    funkcja srednia

--- avg.php
+++ avg.php
@@ -1,16 +1,7 @@
 <?php
 function srednia($liczby) {
     if (empty($liczby)) {
-        return 0; // lub: throw new Exception("Tablica jest pusta");
+        return 0;
     }
-
-    $suma = array_sum($liczby);
-    $ilosc = count($liczby);
-
-    return $suma / $ilosc;
+    return array_sum($liczby) / count($liczby);
 }
-
-// Przykład użycia:
-$lista = [3, 5, 7, 10];
-echo "Średnia: " . srednia($lista);  // Wynik: Średnia: 6.25
-?>
